7b6e98d09e0afc422e53efefaa51f0d5cfc92f17
Zealous Macwan <Macwan.Zealous@philips.com>
2022-12-21 16:31:46 +0530

    added-props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, {Component} from 'react'
 import './App.css'
 import Greet from './components/Greet'
-import Welcome from './components/Welcome'
-import Hello from './components/Hello'
+import Welcome from "./components/Welcome";
 
 class App extends Component {
   render(){
     return(
       <div className="App">
-        <Greet></Greet>
-        <Welcome></Welcome>
-        <Hello></Hello>
+        <Greet name="Zee" heroName="Batman">
+          <p>This is child element</p>
+        </Greet>
+        <Greet name="Mac" heroName="Superman">
+          <button>Button</button>
+        </Greet>
+        <Greet name="Zees" heroName="Spiderman"></Greet>
+        <Welcome name="Zee">
+          <p>This is class child</p>
+        </Welcome>
+        <Welcome name="mac"></Welcome>
       </div>
     );
   }
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
-// function Greet(params) {
-//     return <h1>Hello Zealous</h1>
-// }
+const Greet = (props) => {
+    console.log(props)
+    return(
+    <div>
+        <h1>Hello { props.name } a.k.a { props.heroName} </h1>
+        {props.children}
+    </div>
+    )
 
-//below function is using es6 arror function syntax
-const Greet = () => <h1>Hello Zealous from new syntax</h1>
+}   
 
-//adding default name will always export this so that during import you can give any name which by
-//default refers to the default componenet 
-//also you can comment below and use named export directly
 export default Greet
