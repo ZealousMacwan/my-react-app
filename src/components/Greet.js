e24e92de12b4785c3b4802b8100d5d880f0d963b
Zealous Macwan <Macwan.Zealous@philips.com>
2022-12-21 12:38:09 +0530

    created-functional-component

---
+++ src/components/Greet.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// function Greet(params) {
+//     return <h1>Hello Zealous</h1>
+// }
+
+//below function is using es6 arror function syntax
+export const Greet = () => <h1>Hello Zealous from new syntax</h1>
+
+//adding default name will always export this so that during import you can give any name which by
+//default refers to the default componenet 
+//also you can comment below and use named export directly
+//export default Greet
