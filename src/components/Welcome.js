b0396b61e629f9eaf01bcc37fea22b5883298b65
Zealous Macwan <Macwan.Zealous@philips.com>
2022-12-21 13:05:15 +0530

    added-class-component

---
+++ src/components/Welcome.js
@@ -0,0 +1,10 @@
+import React, { Component  } from 'react'
+
+class Welcome extends Component {
+    
+    //must have render which returns html or atleast NULL
+    render(){
+        return <h1> Welcome from Class componenet</h1>
+    }
+}
+export default Welcome
