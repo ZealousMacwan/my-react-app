ad940f7708ec01ffafbefb807e32c10e40757405
Zealous Macwan <Macwan.Zealous@philips.com>
2022-12-21 22:01:40 +0530

    implemented-state

--- src/App.js
+++ src/App.js
@@ -2,22 +2,15 @@ import React, {Component} from 'react'
 import './App.css'
 import Greet from './components/Greet'
 import Welcome from "./components/Welcome";
+import Message from "./components/Message";
+
 
 class App extends Component {
   render(){
     return(
       <div className="App">
-        <Greet name="Zee" heroName="Batman">
-          <p>This is child element</p>
-        </Greet>
-        <Greet name="Mac" heroName="Superman">
-          <button>Button</button>
-        </Greet>
-        <Greet name="Zees" heroName="Spiderman"></Greet>
-        <Welcome name="Zee">
-          <p>This is class child</p>
-        </Welcome>
-        <Welcome name="mac"></Welcome>
+        <Message></Message>
+        
       </div>
     );
   }
