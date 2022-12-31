e2d4436e3a1ce9d739a632c7d9781401efda0859
Zealous Macwan <Macwan.Zealous@philips.com>
2022-12-31 16:16:33 +0530

    implemented-destructuring-props-state

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,9 +4,11 @@ class Welcome extends Component {
     
     //must have render which returns html or atleast NULL
     render(){
+        const {name, heroName} = this.props
+        //const {state1, state2} = this.state
         return(
             <div>
-                <h1> Welcome {this.props.name} from Class componenet</h1>
+                <h1> Welcome {name} from Class componenet {heroName}</h1>
                 {this.props.children}            
             </div>
         )
