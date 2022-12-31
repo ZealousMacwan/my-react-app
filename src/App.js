417c520e054c1cf83ce69038b367cb931b522d2c
Zealous Macwan <Macwan.Zealous@philips.com>
2022-12-31 16:54:58 +0530

    event-handling-simple

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import Greet from './components/Greet'
 import Welcome from "./components/Welcome";
 import Message from "./components/Message";
 import Counter from './components/Counter'
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 
 class App extends Component {
   render(){
     return(
       <div className="App">
-        <Greet name="Zealous" heroName="Zee"></Greet>
-        <Welcome name="Zee" heroName="Z"></Welcome>
+        <FunctionClick myname="Zealous"></FunctionClick>
+        <ClassClick></ClassClick>
       </div>
     )
   }
